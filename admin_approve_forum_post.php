1e3997390aa3364d440181171668911f983bcffe
and703 <andwishyou@gmail.com>
2017-12-21 23:45:22 +0700

    update

--- admin_approve_forum_post.php
+++ admin_approve_forum_post.php
@@ -182,56 +182,64 @@
 			<!--awal isi-->	
 			<?php
 
-include "admin_connection.php"; 
-//include "admin_auth.php";
-IF (isset($_POST['submit_ap'])) : 
-  $arrDEL = array(); 
-  $arrAPP = array(); 
-  FOREACH ($_POST['action'] as $pf_id => $thisACTION) : 
-    IF ($thisACTION == "APP") : 
-      $arrAPP[] = $pf_id; 
-    ELSE : 
-      $arrDEL[] = $pf_id; 
-    ENDIF; 
-  ENDFOREACH; 
-  IF ($arrDEL) : 
-    $sql_in = implode(",", $arrDEL); 
-    $sql  = " DELETE FROM `pforum` WHERE `pf_id` IN ($sql_in) "; 
-    $qry  = mysqli_query($con, $sql) or die("SQL Error: $sql<br>" . mysqli_error($con));
-  ENDIF; 
-  IF ($arrAPP) : 
-    $sql_in = implode(",", $arrAPP); 
-    $sql  = " UPDATE `pforum` SET `pf_status`=1 WHERE `pf_id` IN ($sql_in) "; 
-    $qry  = mysqli_query($con, $sql) or die("SQL Error: $sql<br>" . mysqli_error($con));
-  ENDIF;
-ENDIF; 
+			include "admin_connection.php"; 
+			//include "admin_auth.php";
+			IF (isset($_POST['submit_ap'])) : 
+			  $arrDEL = array(); 
+			  $arrAPP = array(); 
+			  FOREACH ($_POST['action'] as $pf_id => $thisACTION) : 
+				IF ($thisACTION == "APP") : 
+				  $arrAPP[] = $pf_id; 
+				ELSE : 
+				  $arrDEL[] = $pf_id; 
+				ENDIF; 
+			  ENDFOREACH; 
+			  IF ($arrDEL) : 
+				$sql_in = implode(",", $arrDEL); 
+				$sql  = " DELETE FROM `pforum` WHERE `pf_id` IN ($sql_in) "; 
+				$qry  = mysqli_query($con, $sql) or die("SQL Error: $sql<br>" . mysqli_error($con));
+			  ENDIF; 
+			  IF ($arrAPP) : 
+				$sql_in = implode(",", $arrAPP); 
+				$sql  = " UPDATE `pforum` SET `pf_status`=1 WHERE `pf_id` IN ($sql_in) "; 
+				$qry  = mysqli_query($con, $sql) or die("SQL Error: $sql<br>" . mysqli_error($con));
+			  ENDIF;
+			ENDIF; 
 
-$sql  = " SELECT `pf_id`, `pf_name`,`pf_title`,`pf_ppost`, `pf_username`, DATE_FORMAT(`pf_date`, '%M %D, %Y at %H:%i') as `pf_date` FROM `pforum` "; 
-$sql .= " WHERE `pf_status`=0 ORDER BY `pf_id` "; 
-$qry  = mysqli_query($con, $sql) or die("SQL Error: $sql<br>" . mysqli_error($con)); 
+			$sql  = " SELECT `pf_id`, `pf_name`,`pf_title`,`pf_ppost`, `pf_username`, DATE_FORMAT(`pf_date`, '%M %D, %Y at %H:%i') as `pf_date` FROM `pforum` "; 
+			$sql .= " WHERE `pf_status`=0 ORDER BY `pf_id` "; 
+			$qry  = mysqli_query($con, $sql) or die("SQL Error: $sql<br>" . mysqli_error($con)); 
+			?>
+			List of Requests pending approval:<br>
+			<form method="post" action="<?php{$_SERVER['PHP_SELF']}?>">
+				<div class="panel-body">
+					<table border="2" class="table table-striped table-hover">
+						<thead>
+						  <tr>
+							  <th>Approve</th>
+							  <th>Remove</th>
+							  <th width=1080>Forwarded Post</th>
+						  </tr>
+						</thead>
+			<?php
+			WHILE ($row = mysqli_fetch_array($qry)) : 
+			?>
+						  <tr>
+							  <td><input type='radio' name='action<?php[{$row['pf_id']}]?>' value='APP'></td>
+							  <td><input type='radio' name='action<?php[{$row['pf_id']}]?>' value='DEL'></td>
+							  <td><strong>Title :<?php echo $row['pf_title']; ?></strong>
+							  <br><strong>Main Post :</strong><br><?php echo $row['pf_ppost'];?>
+							  <br>&nbsp;&nbsp;(Submitted by <?php echo $row['pf_name'] . " on " . $row['pf_date']; ?>)</td>
+						  </tr>
+			<?php
+			ENDWHILE; 
+			?>
+					</table>
+					<input type='submit' name='submit_ap' value='Process'>
+				</div>
+			</form> 
 
-print "List of Requests pending approval:<br>"; 
-print "<form method='post' action='{$_SERVER['PHP_SELF']}'>"; 
-print "<table border=1>"; 
-  print "<tr>"; 
-  print "<td>Approve</td>"; 
-  print "<td>Remove</td>"; 
-  print "<td width=1080>Forwarded Post</td>"; 
-  print "</tr>"; 
-WHILE ($row = mysqli_fetch_array($qry)) : 
-  print "<tr>"; 
-  print "<td><input type='radio' name='action[{$row['pf_id']}]' value='APP'></td>";
-  print "<td><input type='radio' name='action[{$row['pf_id']}]' value='DEL'></td>";
-  print "<td><strong>Title :" . $row['pf_title'] . "</strong>"; 
-  print "<br>". "<strong>Main Post :</strong><br>".$row['pf_ppost'];
-  print "<br>&nbsp;&nbsp;(Submitted by " . $row['pf_name'] . " on " . $row['pf_date'] . ")</td>"; 
-  print "</tr>"; 
-ENDWHILE; 
-print "</table>"; 
-print "<input type='submit' name='submit_ap' value='Process'>"; 
-print "</form>"; 
 
-?>
   
 			<!--akhir isi-->	
 			
